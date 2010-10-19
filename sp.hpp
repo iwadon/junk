c189765714b351d21df666de7bcfc6a34de1db2e
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2010-10-19 16:04:42 +0900

    * sp.hpp: StringPieceのようなクラス

---
+++ sp.hpp
@@ -0,0 +1,28 @@
+// StringPiece class.
+// This code is based on Google Chrome's StringPiece class.
+
+#ifndef SP_HPP_INCLUDED
+#define SP_HPP_INCLUDED 1
+
+#include <cstddef>
+#include <cstring>
+#include <string>
+
+class SP
+{
+public:
+  SP() : ptr_(NULL), len_(0) {}
+  SP(const char *str) : ptr_(str), len_(str == NULL ? 0 : strlen(str)) {}
+  SP(const std::string &str) : ptr_(str.data()), len_(str.size()) {}
+  const char *data() const { return ptr_; }
+  size_t size() const { return len_; }
+  const char *c_str() const { return ptr_; }
+  bool empty() const { return len_ == 0; }
+  bool operator==(const SP &x) const { return (len_ == x.len_) || (memcmp(ptr_, x.ptr_, len_) == 0); }
+  void CopyToString(std::string *target) const { target->assign(empty() ? "" : ptr_, len_); }
+private:
+  const char *ptr_;
+  size_t len_;
+};
+
+#endif // !defined(SP_HPP_INCLUDED)
