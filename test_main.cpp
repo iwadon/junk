f6475aa557894a3007a3dc66b291f8eab5e587d3
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-01-08 23:44:25 +0900

    * libtest_main.aを追加。 * テスト中にログ出力を極力出さないようにした。

--- test_main.cpp
+++ test_main.cpp
@@ -3,9 +3,11 @@
 #endif
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
+#include "logger.hpp"
 
 int main()
 {
+  Logger::get_instance().set_level(Logger::LEVEL_FATAL);
   CppUnit::TextUi::TestRunner runner;
   runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
   return runner.run() ? 0 : 1;
