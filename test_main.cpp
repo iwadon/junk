1a1d25b0592dfd72690858475df62bf524996b3d
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-01-09 07:07:59 +0900

    * test_main.cpp (main): CppUnit::TextTestRunnerクラスを使うようにした。

--- test_main.cpp
+++ test_main.cpp
@@ -1,14 +1,14 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
-#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/ui/text/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include "logger.hpp"
 
 int main()
 {
   Logger::get_instance().set_level(Logger::LEVEL_FATAL);
-  CppUnit::TextUi::TestRunner runner;
+  CppUnit::TextTestRunner runner;
   runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
   return runner.run() ? 0 : 1;
 }
