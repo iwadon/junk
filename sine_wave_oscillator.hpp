f034c2b30d3f006467dfbb812cff2def238666b7
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-01-20 13:57:51 +0900

    * sine_wave_oscillator.hpp (SineWaveOscillator): ドキュメントを書き始めた。

--- sine_wave_oscillator.hpp
+++ sine_wave_oscillator.hpp
@@ -3,6 +3,23 @@
 
 #include "oscillator.hpp"
 
+/**
+ * @brief 正弦波(サイン波)を生成するためのクラス
+ *
+ * @sa Oscillator
+ * @sa TriangleWaveOscillator
+ * @sa SawWaveOscillator
+ * @sa SquareWaveOscillator
+ *
+ * @code
+ * SineWaveOscillator osc;
+ * osc.set_sample_rate(44100);
+ * osc.set_frequency(440);
+ * while (true) {
+ *   osc.value();
+ * }
+ * @endcode
+ */
 class SineWaveOscillator : public Oscillator
 {
 public:
