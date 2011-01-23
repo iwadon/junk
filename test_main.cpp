8fc62b039d29ab007e623d74d2f41c6bc1170d03
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-01-23 21:28:55 +0900

    * test_main.cpp: テストプログラムの引数にテストパス(PointTestやPointTest::test_CTORなど)を指定できるようにした。

--- test_main.cpp
+++ test_main.cpp
@@ -2,8 +2,16 @@
 #include "config.h"
 #endif
 #include <iostream>
+#include <string>
 #include <cppunit/ui/text/TextTestRunner.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TextOutputter.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TextTestProgressListener.h>
+#ifdef HAVE_BOOST
+#include <boost/foreach.hpp>
+#endif
 #ifdef HAVE_BOOST_PROGRAM_OPTIONS
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
@@ -12,13 +20,16 @@ namespace po = boost::program_options;
 
 int main(int argc, char *argv[])
 {
-  po::options_description opt(std::string("Usage: ") + argv[0] + " [Options]\nOptions");
+  po::options_description opt(std::string("Usage: ") + argv[0] + " [Options] [Test Path] ...\nOptions");
   opt.add_options()
     ("help,h", "Print this help")
-    ("verbose,v", "Verbose mode");
+    ("verbose,v", "Verbose mode")
+    ("test-path,t", po::value<std::vector<std::string> >(), "Specify the test path");
+  po::positional_options_description p;
+  p.add("test-path", -1);
   po::variables_map vars;
   try {
-    po::store(po::parse_command_line(argc, argv, opt), vars);
+    po::store(po::command_line_parser(argc, argv).options(opt).positional(p).run(), vars);
     po::notify(vars);
     if (vars.count("help") != 0) {
       std::cerr << opt << std::endl;
@@ -34,5 +45,26 @@ int main(int argc, char *argv[])
   }
   CppUnit::TextTestRunner runner;
   runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
-  return runner.run() ? 0 : 1;
+  if (vars.count("test-path") > 0) {
+    CppUnit::TestResult controller;
+    CppUnit::TestResultCollector result;
+    controller.addListener(&result);
+    CppUnit::TextTestProgressListener progress;
+    controller.addListener(&progress);
+    const std::vector<std::string> &v = vars["test-path"].as<std::vector<std::string> >();
+    BOOST_FOREACH(std::string i, v) {
+      std::cout << i << " ";
+      try {
+	runner.run(controller, i);
+      } catch (std::invalid_argument &e) {
+	std::cerr << "ERROR: " << e.what() << std::endl;
+	return 1;
+      }
+    }
+    CppUnit::TextOutputter outputter(&result, std::cerr);
+    outputter.write();
+    return result.wasSuccessful() ? 0 : 1;
+  } else {
+    return runner.run() ? 0 : 1;
+  }
 }
