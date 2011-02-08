0369872cbed2978ae57f3039c59359f7fc7091a7
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-02-08 23:43:47 +0900

    * texture_pool.hpp: より良いmap系クラスを使うようにした。

--- texture_pool.hpp
+++ texture_pool.hpp
@@ -1,15 +1,18 @@
 #ifndef TEXTURE_POOL_HPP_INCLUDED
 #define TEXTURE_POOL_HPP_INCLUDED 1
 
-#ifdef STDCXX_98_HEADERS
-#include <map>
 #include <string>
-#endif
-#ifdef HAVE_BOOST
 #include <boost/noncopyable.hpp>
 #include <boost/pool/object_pool.hpp>
-#endif
 #include "sp.hpp"
+
+#if defined(HAVE_TR1_UNORDERED_MAP)
+#include <tr1/unordered_map>
+#elif defined(HAVE_BOOST)
+#include <boost/unordered_map.hpp>
+#else
+#include <map>
+#endif
 
 struct Texture;
 
@@ -21,7 +24,13 @@ public:
   void destroy(Texture *tex);
 private:
   typedef boost::object_pool<Texture> texture_pool_type;
+#if defined(HAVE_TR1_UNORDERED_MAP)
+  typedef std::tr1::unordered_map<std::string, Texture *> texture_map_type;
+#elif defined(HAVE_BOOST)
+  typedef boost::unordered_map<const std::string, Texture *> texture_map_type;
+#else
   typedef std::map<const std::string, Texture *> texture_map_type;
+#endif
   texture_pool_type texture_pool_;
   texture_map_type texture_map_;
   texture_map_type::iterator find(const SP &filename);
