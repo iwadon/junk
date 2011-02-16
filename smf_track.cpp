c78ecfea6685ee2ca40a675ddb9cfe5aa447c014
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-02-16 09:53:05 +0900

    * SMFTrackクラスでランニングステータスをオプション扱いにした。 * ログへの出力をオプション扱いにした。 * データの読み進め方を整理した。

--- smf_track.cpp
+++ smf_track.cpp
@@ -4,13 +4,11 @@
 #include "smf_track.hpp"
 #include "channel.hpp"
 #include "instrument.hpp"
-#include "logger.hpp"
 #include "smf.hpp"
-#include "sp.hpp"
 #include "variable_length_value.hpp"
 
 #ifdef SMF_TRACK_DEBUG
-#include <cstring>
+#include "logger.hpp"
 #define SET_STATE(s) set_state(s)
 #else
 #define SET_STATE(s) state_ = (s)
@@ -45,102 +43,153 @@ void SMFTrack::update()
     return;
   }
   do {
-    data_type data = *data_cur_;
+    data_type data = *data_cur_++;
     Channel &ch = smf_.instrument().channel(data & 0x0f);
+#ifdef ENABLE_RUNNING_STATUS
   retry:
+#endif
     switch (static_cast<unsigned char>(data)) {
-    case 0x00 ... 0x7f:
-      // Running Status
+#ifdef ENABLE_RUNNING_STATUS
+    case 0x00 ... 0x7f:		// Running Status
       data = prev_status_byte;
-      --data_cur_;
       goto retry;
       //break;
-    case 0x80 ... 0x8f:
-      ch.note_off(data_cur_[1], data_cur_[2]);
-      data_cur_ += 3;
-      assert(data_cur_ <= data_end_);
+#endif
+    case 0x80 ... 0x8f:		// 8n kk vv   Note Off
+      {
+	data_type kk = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	data_type vv = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.note_off(kk, vv);
+      }
       break;
-    case 0x90 ... 0x9f:
-      ch.note_on(data_cur_[1], data_cur_[2]);
-      data_cur_ += 3;
-      assert(data_cur_ <= data_end_);
+    case 0x90 ... 0x9f:		// 9n kk vv   Note On
+      {
+	data_type kk = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	data_type vv = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.note_on(kk, vv);
+      }
       break;
-    case 0xa0 ... 0xaf:
-      ch.polyphonic_pressure(data_cur_[1], data_cur_[2]);
-      data_cur_ += 3;
-      assert(data_cur_ <= data_end_);
+    case 0xa0 ... 0xaf:		// An kk vv   Polyphonic Pressure
+      {
+	data_type kk = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	data_type vv = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.polyphonic_pressure(kk, vv);
+      }
       break;
-    case 0xb0 ... 0xbf:
-      ch.control_change(data_cur_[1], data_cur_[2]);
-      data_cur_ += 3;
-      assert(data_cur_ <= data_end_);
+    case 0xb0 ... 0xbf:		// Bn cc vv   Control Change
+      {
+	data_type cc = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	data_type vv = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.control_change(cc, vv);
+      }
       break;
-    case 0xc0 ... 0xcf:
-      ch.program_change(data_cur_[1]);
-      data_cur_ += 2;
-      assert(data_cur_ <= data_end_);
+    case 0xc0 ... 0xcf:		// Cn pp      Program Change
+      {
+	data_type pp = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.program_change(pp);
+      }
       break;
-    case 0xd0 ... 0xdf:
-      ch.channel_pressure(data_cur_[1]);
-      data_cur_ += 2;
-      assert(data_cur_ <= data_end_);
+    case 0xd0 ... 0xdf:		// Dn vv      Channel Pressure
+      {
+	data_type vv = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.channel_pressure(vv);
+      }
       break;
-    case 0xe0 ... 0xef:
-      ch.pitch_bend_change((data_cur_[2] * 128 + data_cur_[1]) - 8192);
-      data_cur_ += 3;
-      assert(data_cur_ <= data_end_);
+    case 0xe0 ... 0xef:		// En ll mm   Pitch Bend
+      {
+	data_type ll = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	data_type mm = *data_cur_++;
+	assert(data_cur_ <= data_end_);
+	ch.pitch_bend_change(mm * 128 + ll - 8192);
+      }
       break;
-    case 0xf0:
-    case 0xf7:
-      // SysEx event
+    case 0xf0:	       		// F0 ... F7  SysEx
       {
 	VariableLengthValue delta_time;
-	VariableLengthValue::len_type vlv_len = delta_time.set_data(data_cur_ + 1, 0);
-	data_cur_ += vlv_len;
+	VariableLengthValue::len_type vlv_len = delta_time.set_data(data_cur_, 0);
+	data_cur_ += vlv_len + delta_time.value();
+	assert(data_cur_ <= data_end_);
+      }
+    case 0xf7:			// F7 ...     SysEx
+      {
+	VariableLengthValue delta_time;
+	VariableLengthValue::len_type vlv_len = delta_time.set_data(data_cur_, 0);
+	data_cur_ += vlv_len + delta_time.value();
 	assert(data_cur_ <= data_end_);
       }
       break;
     case 0xff:
-      switch (data_cur_[1]) {
-      case 0x01: // FF 01 len text		Text Event
-      case 0x02: // FF 02 len text		Copyright Notice
-      case 0x03: // FF 03 len text		Sequence/Track Name
-      case 0x04: // FF 04 len text		Instrument Name
-      case 0x05: // FF 05 len text		Lyric
-      case 0x06: // FF 06 len text		Marker
-      case 0x07: // FF 07 len text		Cue Point
-      case 0x09: // FF 09 len text		Device Name
-      case 0x20: // FF 20 01 cc			MIDI Channel Prefix
-      case 0x54: // FF 54 05 hr mn se fr ff	SMPTE Offset
-      case 0x58: // FF 58 04 nn dd cc bb	Time Signature
-      case 0x59: // FF 59 02 sf mi		Key Signature
-      case 0x7f: // FF 7F len data		Sequencer Specific Meta-Event
-	// not implemented
-	data_cur_ += 2 + 1 + data_cur_[2];
+      {
+	data_type type = *data_cur_++;
 	assert(data_cur_ <= data_end_);
-	break;
-      case 0x2f: // FF 2F 00			End of Track
-	stop();
-	break;
-      case 0x51: // FF 51 03 tttttt		Set Tempo(in microseconds per MIDI quarter-note)
-	smf_.set_tempo(reinterpret_cast<const uint8_t *>(data_cur_) + 3);
-	data_cur_ += 2 + 1 + 3;
+	VariableLengthValue data_len;
+	VariableLengthValue::len_type vlv_len = data_len.set_data(data_cur_, 0);
+	data_cur_ += vlv_len;
 	assert(data_cur_ <= data_end_);
-	break;
-      default:
-	// unsupported meta-events
-	INFO("Unimplemented meta events: %02x", static_cast<uint8_t>(data_cur_[1]));
-	data_cur_ += 2 + 1 + data_cur_[2];
-	assert(data_cur_ <= data_end_);
-	break;
+	switch (type) {
+	case 0x01: // FF 01 len text		Text Event
+	case 0x02: // FF 02 len text		Copyright Notice
+	case 0x03: // FF 03 len text		Sequence/Track Name
+	case 0x04: // FF 04 len text		Instrument Name
+	case 0x05: // FF 05 len text		Lyric
+	case 0x06: // FF 06 len text		Marker
+	case 0x07: // FF 07 len text		Cue Point
+	case 0x09: // FF 09 len text		Device Name
+	case 0x20: // FF 20 01 cc		MIDI Channel Prefix
+	case 0x54: // FF 54 05 hr mn se fr ff	SMPTE Offset
+	case 0x58: // FF 58 04 nn dd cc bb	Time Signature
+	case 0x59: // FF 59 02 sf mi		Key Signature
+	case 0x7f: // FF 7F len data		Sequencer Specific Meta-Event
+	  // not implemented
+	  data_cur_ += data_len.value();
+	  assert(data_cur_ <= data_end_);
+	  break;
+	case 0x2f: // FF 2F 00			End of Track
+#ifdef SMF_TRACK_DEBUG
+	  if (data_len.value() > 0) {
+	    WARN("End of Track event is too long: %u > 0", data_len.value());
+	  }
+#endif
+	  data_cur_ += data_len.value();
+	  stop();
+	  break;
+	case 0x51: // FF 51 03 tttttt		Set Tempo(in microseconds per MIDI quarter-note)
+	  smf_.set_tempo(reinterpret_cast<const uint8_t *>(data_cur_));
+	  data_cur_ += data_len.value();
+	  assert(data_cur_ <= data_end_);
+	  break;
+	default:
+	  // unsupported meta-events
+#ifdef SMF_TRACK_DEBUG
+	  INFO("Unimplemented meta events: type=%02x, len=%u", static_cast<uint8_t>(type), data_len.value());
+#endif
+	  data_cur_ += data_len.value();
+	  assert(data_cur_ <= data_end_);
+	  break;
+	}
       }
       break;
     default:
       // unsupported events
+#ifdef SMF_TRACK_DEBUG
       INFO("Unimplemented events: %02x", static_cast<uint8_t>(data));
+#endif
       break;
     }
+#ifdef ENABLE_RUNNING_STATUS
     prev_status_byte = data;
+#endif
     if (!is_playing()) {
       break;
     }
@@ -151,17 +200,23 @@ void SMFTrack::update()
 bool SMFTrack::setup(const data_type *data, const size_t size)
 {
   if (size < 8) {
+#ifdef SMF_TRACK_DEBUG
     ERROR("Too short data");
+#endif
     return false;
   }
   if (memcmp(data, "MTrk", 4) != 0) {
+#ifdef SMF_TRACK_DEBUG
     ERROR("Not MTrk data");
+#endif
     return false;
   }
   const uint8_t *d = reinterpret_cast<const uint8_t *>(data);
   size_t data_size = d[4] << 24 | d[5] << 16 | d[6] << 8 | d[7];
   if (data_size != size - 8) {
+#ifdef SMF_TRACK_DEBUG
     ERROR("mismatch data size: %zu %zu %02x%02x%02x%02x", data_size, size - 8, d[4], d[5], d[6], d[7]);
+#endif
     return false;
   }
   data_ = data + 8;
@@ -217,14 +272,14 @@ void SMFTrack::update_wait_time()
   wait_time_ += delta_time.value();
 }
 
+#ifdef SMF_TRACK_DEBUG
+
 static const char *state_string[SMFTrack::NUM_STATES] = {
   "NONE",
   "INITIALIZED",
   "PLAYING",
   "STOPPED",
 };
-
-#ifdef SMF_TRACK_DEBUG
 
 std::string SMFTrack::inspect() const
 {
