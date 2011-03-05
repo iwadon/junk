50f5d4bab0005935f0da9d26d9d6444930d243a7
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-03-05 21:44:36 +0900

    * load_time_test.cpp: sleep(1)をusleep(100000)に差し替えた。

--- load_time_test.cpp
+++ load_time_test.cpp
@@ -27,32 +27,32 @@ void LoadTimeTest::test_elapsed_time()
   LoadTime lt;
   lt.activate(0);
   lt.start(0);
-  sleep(1);
+  usleep(10000);
   lt.stop(0);
   lt.flip();
-  ASSERT_TIME(1.0, lt.elapsed_time(0));
+  ASSERT_TIME(0.01, lt.elapsed_time(0));
 }
 
 void LoadTimeTest::test_activate()
 {
   LoadTime lt;
-  lt.start(0); sleep(1); lt.stop(0);
-  lt.start(1); sleep(1); lt.stop(1);
+  lt.start(0); usleep(10000); lt.stop(0);
+  lt.start(1); usleep(10000); lt.stop(1);
   lt.flip();
   ASSERT_TIME(0.0, lt.elapsed_time(0));
   ASSERT_TIME(0.0, lt.elapsed_time(1));
   lt.activate(0);
-  lt.start(0); sleep(1); lt.stop(0);
-  lt.start(1); sleep(1); lt.stop(1);
+  lt.start(0); usleep(10000); lt.stop(0);
+  lt.start(1); usleep(10000); lt.stop(1);
   lt.flip();
-  ASSERT_TIME(1.0, lt.elapsed_time(0));
+  ASSERT_TIME(0.01, lt.elapsed_time(0));
   ASSERT_TIME(0.0, lt.elapsed_time(1));
   lt.activate(1);
-  lt.start(0); sleep(1); lt.stop(0);
-  lt.start(1); sleep(1); lt.stop(1);
+  lt.start(0); usleep(10000); lt.stop(0);
+  lt.start(1); usleep(10000); lt.stop(1);
   lt.flip();
-  ASSERT_TIME(1.0, lt.elapsed_time(0));
-  ASSERT_TIME(1.0, lt.elapsed_time(1));
+  ASSERT_TIME(0.01, lt.elapsed_time(0));
+  ASSERT_TIME(0.01, lt.elapsed_time(1));
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(LoadTimeTest);
