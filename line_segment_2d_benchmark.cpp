3325fbac490a1424af566d1ab1040713818f71f0
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-03-22 06:08:01 +0900

    * line_segment_2d_benchmark.cpp: マクロを1行で書けるようにした。

--- line_segment_2d_benchmark.cpp
+++ line_segment_2d_benchmark.cpp
@@ -6,6 +6,8 @@
 #include "benchmark.hpp"
 #include "point_2d.hpp"
 
+#define B(code) BM(code, {r = code;})
+
 int main()
 {
   static bool r;
@@ -16,50 +18,25 @@ int main()
   LineSegment2D l5(1.0f, 0.0f, 0.6f, 0.4f);
   LineSegment2D l6(2.0f, 1.0f, 1.0f, 2.0f);
   Point2D cp;
-  BM(l1.is_crossed(l2), {
-      r = l1.is_crossed(l2);
-    });
-  BM(l1.cross_point(l2), {
-      r = l1.cross_point(l2);
-    });
-  BM(l1.intersection_point_with(cp, l2), {
-      r = l1.intersection_point_with(cp, l2);
-    });
-  BM(l1.is_crossed(l3), {
-      r = l1.is_crossed(l3);
-    });
-  BM(l1.cross_point(l3), {
-      r = l1.cross_point(l3);
-    });
-  BM(l1.intersection_point_with(cp, l3), {
-      r = l1.intersection_point_with(cp, l3);
-    });
-  BM(l1.is_crossed(l4), {
-      r = l1.is_crossed(l4);
-    });
-  BM(l1.cross_point(l4), {
-      r = l1.cross_point(l4);
-    });
-  BM(l1.intersection_point_with(cp, l4), {
-      r = l1.intersection_point_with(cp, l4);
-    });
-  BM(l1.is_crossed(l5), {
-      r = l1.is_crossed(l5);
-    });
-  BM(l1.cross_point(l5), {
-      r = l1.cross_point(l5);
-    });
-  BM(l1.intersection_point_with(cp, l5), {
-      r = l1.intersection_point_with(cp, l5);
-    });
-  BM(l1.is_crossed(l6), {
-      r = l1.is_crossed(l6);
-    });
-  BM(l1.cross_point(l6), {
-      r = l1.cross_point(l6);
-    });
-  BM(l1.intersection_point_with(cp, l6), {
-      r = l1.intersection_point_with(cp, l6);
-    });
+  B(l1.is_crossed(l2));
+  B(l1.intersection_point_with(cp, l2));
+  B(l1.cross_point(l2));
+  B(l1.cross_point(l2, &cp));
+  B(l1.is_crossed(l3));
+  B(l1.intersection_point_with(cp, l3));
+  B(l1.cross_point(l3));
+  B(l1.cross_point(l3, &cp));
+  B(l1.is_crossed(l4));
+  B(l1.intersection_point_with(cp, l4));
+  B(l1.cross_point(l4));
+  B(l1.cross_point(l4, &cp));
+  B(l1.is_crossed(l5));
+  B(l1.intersection_point_with(cp, l5));
+  B(l1.cross_point(l5));
+  B(l1.cross_point(l5, &cp));
+  B(l1.is_crossed(l6));
+  B(l1.intersection_point_with(cp, l6));
+  B(l1.cross_point(l6));
+  B(l1.cross_point(l6, &cp));
   return 0;
 }
