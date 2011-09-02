0094f342843a22a7913eb13d62ec696bf04ee5b8
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2011-09-02 22:11:46 +0900

    CharとBEGINとENDを追加。

--- peg_sample.cpp
+++ peg_sample.cpp
@@ -10,6 +10,13 @@
 class PegSample : public ::testing::Test
 {
 public:
+  static peg::Rule Identifier;
+  static peg::Rule IdentStart;
+  static peg::Rule IdnetCont;
+  static peg::Rule Literal;
+  static peg::Rule Class;
+  static peg::Rule Range;
+  static peg::Rule Char;
   static peg::Rule LEFTARROW;
   static peg::Rule SLASH;
   static peg::Rule AND;
@@ -30,6 +37,20 @@ public:
   static peg::Rule END;
 };
 
+peg::Rule PegSample::Identifier = !peg::any;
+peg::Rule PegSample::IdentStart = !peg::any;
+peg::Rule PegSample::IdnetCont = !peg::any;
+peg::Rule PegSample::Literal = !peg::any;
+peg::Rule PegSample::Class = !peg::any;
+peg::Rule PegSample::Range = !peg::any;
+peg::Rule PegSample::Char =
+  (peg::char_('\\') >> (peg::char_('a') / peg::char_('b') / peg::char_('e') / peg::char_('f')
+			/ peg::char_('n') / peg::char_('r') / peg::char_('t') / peg::char_('v')
+			/ peg::char_('\'') / peg::char_('"') / peg::char_('[') / peg::char_(']') / peg::char_('\\')))
+  / (peg::char_('\\') >> peg::range('0', '3') >> peg::range('0', '7') >> peg::range('0', '7'))
+  / (peg::char_('\\') >> peg::range('0', '7') >> peg::range('0', '7'))
+  / peg::str("\\-")
+  / ((!peg::char_('\\')) >> peg::any);
 peg::Rule PegSample::LEFTARROW = peg::str("<-") >> Spacing;
 peg::Rule PegSample::SLASH = peg::char_('/') >> Spacing;
 peg::Rule PegSample::AND = peg::char_('&') >> Spacing;
@@ -45,9 +66,72 @@ peg::Rule PegSample::Comment = peg::char_('#') >> *(!EndOfLine >> peg::any) >> E
 peg::Rule PegSample::Space = peg::char_(' ') / peg::char_('\t') / EndOfLine;
 peg::Rule PegSample::EndOfLine = peg::str("\r\n") / peg::char_('\n') / peg::char_('\r');
 peg::Rule PegSample::EndOfFile = !peg::any;
+
 peg::Rule PegSample::Action = peg::char_('{') >> *(!peg::char_('}') >> peg::any) >> peg::char_('}') >> Spacing;
-peg::Rule PegSample::BEGIN = !peg::any;
-peg::Rule PegSample::END = !peg::any;
+peg::Rule PegSample::BEGIN = peg::char_('<') >> Spacing;
+peg::Rule PegSample::END = peg::char_('>') >> Spacing;
+
+TEST_F(PegSample, Identifier)
+{
+}
+
+TEST_F(PegSample, IdentStart)
+{
+}
+
+TEST_F(PegSample, IdnetCont)
+{
+}
+
+TEST_F(PegSample, Literal)
+{
+}
+
+TEST_F(PegSample, Class)
+{
+}
+
+TEST_F(PegSample, Range)
+{
+}
+
+TEST_F(PegSample, Char)
+{
+  peg::Result result = peg::parse(+Char, "\\a\\b\\e\\f\\n\\r\\t\\v\\\'\\\"\\[\\]\\\\");
+  EXPECT_EQ(true, result.status);
+  EXPECT_STREQ("", result.rest);
+
+  for (int i = 0; i <= 0377; ++i) {
+    char buf[5];
+    snprintf(buf, sizeof buf, "\\%03o", i);
+    result = peg::parse(Char, buf);
+    EXPECT_EQ(true, result.status);
+    EXPECT_STREQ("", result.rest);
+  }
+
+  for (int i = 0; i <= 077; ++i) {
+    char buf[4];
+    snprintf(buf, sizeof buf, "\\%02o", i);
+    result = peg::parse(Char, buf);
+    EXPECT_EQ(true, result.status);
+    EXPECT_STREQ("", result.rest);
+  }
+
+  result = peg::parse(Char, "\\-");
+  EXPECT_EQ(true, result.status);
+  EXPECT_STREQ("", result.rest);
+
+  for (int i = 1; i <= 0xff; ++i) {
+    if (i == '\\') {
+      continue;
+    }
+    char buf[2];
+    snprintf(buf, sizeof buf, "%c", i);
+    result = peg::parse(Char, buf);
+    EXPECT_EQ(true, result.status);
+    EXPECT_STREQ("", result.rest);
+  }
+}
 
 TEST_F(PegSample, LEFTARROW)
 {
@@ -186,8 +270,14 @@ TEST_F(PegSample, Action)
 
 TEST_F(PegSample, BEGIN)
 {
+  peg::Result result = peg::parse(BEGIN, "< \t\r\n");
+  EXPECT_EQ(true, result.status);
+  EXPECT_STREQ("", result.rest);
 }
 
 TEST_F(PegSample, END)
 {
+  peg::Result result = peg::parse(END, "> \t\r\n");
+  EXPECT_EQ(true, result.status);
+  EXPECT_STREQ("", result.rest);
 }
