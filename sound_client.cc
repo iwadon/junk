e56778024ffb3c3ab29fa1e1d5a7b1cf0c3e4b55
Hiroyuki Iwatsuki <don@na.rim.or.jp>
2012-10-03 03:10:34 +0900

    ボタンとかを追加。

--- sound_client.cc
+++ sound_client.cc
@@ -22,18 +22,24 @@ public:
   ~SoundClientFrame();
   void OnQuit(wxCommandEvent &event);
   void OnAbout(wxCommandEvent &event);
+  void OnConnectButtonClicked(wxCommandEvent &event);
+private:
+  wxButton *connect_button_;
 protected:
   DECLARE_EVENT_TABLE()
 };
 
 enum {
+  ID_CONNECT_BUTTON = 1,
+
   MENU_QUIT = wxID_EXIT,
   MENU_ABOUT = wxID_ABOUT,
 };
 
 BEGIN_EVENT_TABLE(SoundClientFrame, wxFrame)
-EVT_MENU(MENU_QUIT, SoundClientFrame::OnQuit)
-EVT_MENU(MENU_ABOUT, SoundClientFrame::OnAbout)
+  EVT_MENU(MENU_QUIT, SoundClientFrame::OnQuit)
+  EVT_MENU(MENU_ABOUT, SoundClientFrame::OnAbout)
+  EVT_BUTTON(ID_CONNECT_BUTTON, SoundClientFrame::OnConnectButtonClicked)
 END_EVENT_TABLE()
 
 IMPLEMENT_APP(SoundClientApp)
@@ -49,19 +55,26 @@ bool SoundClientApp::OnInit()
 }
 
 SoundClientFrame::SoundClientFrame()
-  : wxFrame(NULL, wxID_ANY, wxT("wx_sample"), wxDefaultPosition, wxSize(800, 600))
+  : wxFrame(NULL, wxID_ANY, wxT("Sound Client"), wxDefaultPosition, wxDefaultSize)
 {
-  wxMenu *menu_file = new wxMenu;
-  menu_file->Append(MENU_QUIT, "E&xit", "Quit this program");
-
-  wxMenu *menu_help = new wxMenu;
-  menu_help->Append(MENU_ABOUT, "&About", "Show about dialog");
-
+  // menu Bar
   wxMenuBar *menubar = new wxMenuBar;
-  menubar->Append(menu_file, "&File");
-  menubar->Append(menu_help, "&Help");
-
+  wxMenu *menu = new wxMenu;
+  menu->Append(MENU_QUIT, "E&xit", "Quit this program");
+  menubar->Append(menu, "&File");
+  menu = new wxMenu;
+  menu->Append(MENU_ABOUT, "&About", "Show about dialog");
+  menubar->Append(menu, "&Help");
   SetMenuBar(menubar);
+
+  wxPanel *panel = new wxPanel(this, wxID_ANY);
+
+  wxBoxSizer *sizer = new wxBoxSizer(wxHORIZONTAL);
+  connect_button_ = new wxButton(panel, ID_CONNECT_BUTTON, "Connect");
+  sizer->Add(connect_button_, 0, 0, 0);
+  sizer->Add(new wxStaticText(panel, wxID_ANY, wxT("---")), 0, 0, 0);
+  sizer->SetSizeHints(panel);
+  panel->SetSizer(sizer);
 }
 
 SoundClientFrame::~SoundClientFrame()
@@ -87,4 +100,9 @@ void SoundClientFrame::OnAbout(wxCommandEvent &WXUNUSED(event))
                  "About wxWidgets minimal sample",
                  wxOK | wxICON_INFORMATION,
                  this);
+}
+
+void SoundClientFrame::OnConnectButtonClicked(wxCommandEvent &event)
+{
+  connect_button_->SetLabel("OK");
 }
